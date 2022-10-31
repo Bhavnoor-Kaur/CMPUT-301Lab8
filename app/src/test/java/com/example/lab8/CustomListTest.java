3f9725d116314d8158a9efffc2c72e1c1523727d
Bhavnoor Kaur <bhavnoorks@gmail.com>
2022-10-31 15:45:16 -0600

    Added test for deleteCity

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -24,11 +24,30 @@ public class CustomListTest {
         assertEquals(listSize + 1, updated);
     }
 
+    @Test
     public void check_hasCity() {
         City newCity = new City("Vancouver", "BC");
         list.addCity(newCity);
         boolean result = list.hasCity(newCity);
         assertEquals(true, result);
+    }
+
+    @Test
+    public void check_deleteCity() {
+        City newCity1 = new City("Toronto", "ON");
+        City newCity2 = new City("Vancouver", "BC");
+        list = new CustomList(null, new ArrayList<City>());
+        list.addCity(newCity1);
+        list.addCity(newCity2);
+        int prevSize = list.getCount();
+
+        list.deleteCity(newCity2);
+        int newSize = list.getCount();
+        boolean hasCity2 = list.hasCity(newCity2);
+
+        assertEquals(false, hasCity2);
+        assertEquals(prevSize - 1, newSize);
+
     }
 
 }
